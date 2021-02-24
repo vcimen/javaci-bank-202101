0330b56685de797a83916e4a621313519e2a8665
vcimen <vcimen@na.edu>
2021-02-23 22:31:25 -0500

    Javaci Bank Back Office

---
+++ src/main/java/net/javaci/bank202101/JavaciBankApplication.java
@@ -0,0 +1,13 @@
+package net.javaci.bank202101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaciBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaciBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaci/bank202101/javacibank/JavaciBankApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaci.bank202101.javacibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaciBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
